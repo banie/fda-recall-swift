81f3d00e79725fc105ce8185d474e1af79a2ed35
banie <banie.setijoso@gmail.com>
2025-10-10 21:52:56 -0400

    Initial proj dump

---
+++ fdaRecall/fdaRecallTests/fdaRecallTests.swift
@@ -0,0 +1,17 @@
+//
+//  fdaRecallTests.swift
+//  fdaRecallTests
+//
+//  Created by banie setijoso on 2025-10-10.
+//
+
+import Testing
+@testable import fdaRecall
+
+struct fdaRecallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
