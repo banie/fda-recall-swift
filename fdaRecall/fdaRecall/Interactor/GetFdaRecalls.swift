23659a8ace4967508f5d4af5b98e3dab21a811c8
banie <banie.setijoso@gmail.com>
2025-10-13 14:03:11 -0400

    Separate the logic to fetch into an interactor so it can be easily unit tested later

---
+++ fdaRecall/fdaRecall/Interactor/GetFdaRecalls.swift
@@ -0,0 +1,35 @@
+//
+//  GetFdaRecalls.swift
+//  fdaRecall
+//
+//  Created by banie setijoso on 2025-10-13.
+//
+
+protocol GetFdaRecalls {
+    func fetch(skip: Int?, limit: Int?) async -> Result<[FdaRecall], DataError>
+}
+
+class GetFdaRecallsImpl: GetFdaRecalls {
+    private let httpSession: HttpSession
+    
+    init(httpSession: HttpSession) {
+        self.httpSession = httpSession
+    }
+    
+    func fetch(skip: Int? = 0, limit: Int? = 0) async -> Result<[FdaRecall], DataError> {
+        var parameters: [String: Any] = [:]
+        if let skip = skip {
+            parameters["skip"] = skip
+        }
+        if let limit = limit {
+            parameters["limit"] = limit
+        }
+        
+        switch await httpSession.get(parameters: parameters) as Result<FDARecallResponse, DataError> {
+        case .success(let response):
+            return .success(response.results)
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+}
---
+++ fdaRecall/fdaRecall/NetworkEndpoints.swift
@@ -0,0 +1,8 @@
+//
+//  Endpoints.swift
+//  fdaRecall
+//
+//  Created by banie setijoso on 2025-10-13.
+//
+
+let BASE_URL = "https://api.fda.gov/food/enforcement.json"
